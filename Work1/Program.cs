6bd83c76eaf995020ed0ac315e71498b43fb0c8c
Aleksei Piatachkov <pyatachkov.ao@gmail.com>
2023-03-26 09:16:58 +0500

    Added work1

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
+// а какое меньшее.
+
+Console.WriteLine("Напиши первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Напиши второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.Write("a = ");
+Console.WriteLine(a);
+Console.Write("b = ");
+Console.WriteLine(b);
+
+if (a > b)
+  {
+   Console.Write("max =");
+   Console.WriteLine(a);
+  }
+else 
+  {
+   Console.Write("max =");
+   Console.WriteLine(b);
+  }
